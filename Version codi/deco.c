532e936c29ee47ad0136ac22a677a88bbd635937
Mdperez19 <69879158+Mdperez19@users.noreply.github.com>
2021-11-02 21:55:22 -0600

    cambio de macros

--- Version codi/deco.c
+++ Version codi/deco.c
@@ -1,9 +1,5 @@
 #include "huffman.h"
-#define PESOBIT(bpos) 1<<bpos
-#define CONSULTARBIT(var,bpos) (*(unsigned*)&var & PESOBIT(bpos))?1:0
-#define PONE_1(var,bpos) *(unsigned*)&var |= PESOBIT(bpos)
-#define PONE_0(var,bpos) *(unsigned*)&var &= ~(PESOBIT(bpos))
-#define CAMBIA(var,bpos) *(unsigned*)&var ^= PESOBIT(bpos)
+
 
 int main()
 {
--- Version codi/huffman.h
+++ Version codi/huffman.h
@@ -1,5 +1,10 @@
 #include "colaprioridad.h"
 #include <string.h>
+#define PESOBIT(bpos) 1<<bpos
+#define CONSULTARBIT(var,bpos) (*(unsigned*)&var & PESOBIT(bpos))?1:0
+#define PONE_1(var,bpos) *(unsigned*)&var |= PESOBIT(bpos)
+#define PONE_0(var,bpos) *(unsigned*)&var &= ~(PESOBIT(bpos))
+#define CAMBIA(var,bpos) *(unsigned*)&var ^= PESOBIT(bpos)
 #define TAMMAX 60
 
 typedef struct Codigo
