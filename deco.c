644c3b78bbac716f3f475ef39f41adf9233d72dd
AlbertoPC13 <BetucaPC@hotmail.com>
2022-02-12 00:34:04 -0600

    Project updated
    Co-Authored-By: Mdperez19 <69879158+Mdperez19@users.noreply.github.com>
    Co-Authored-By: Mauricio Jesús Pacheco Mijangos <73979932+mauricioPacheco894@users.noreply.github.com>
    Co-Authored-By: Jesus Morales <59420444+Capu720@users.noreply.github.com>

---
+++ deco.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "tiempo.h"
+#include "huffman.h"
+#define PESOBIT(bpos) 1<<bpos
+#define CONSULTARBIT(var,bpos) (*(unsigned*)&var & PESOBIT(bpos))?1:0
+#define PONE_1(var,bpos) *(unsigned*)&var |= PESOBIT(bpos)
+#define PONE_0(var,bpos) *(unsigned*)&var &= ~(PESOBIT(bpos))
+#define CAMBIA(var,bpos) *(unsigned*)&var ^= PESOBIT(bpos)
+
+/*
+Funcion Decodificacion
+Resumen: Descomprime el archivo comprimido
+Errores: Ninguno
+Parametros: Ninguno
+*/
+int main()
+{
+    double sumwtime, utime0, stime0, wtime0,utime1, stime1, wtime1; //Variables para medición de tiempos
+    uswtime(&utime0, &stime0, &wtime0);//Inicia el conteo
+    //Declaracion e inicializacion de variables
+    nodo arbolHuffman;
+    nodo *nodos;
+    TIPO posicion = 0, reps = 0;
+    char cadenaCodigos[TAMMAX];
+    char *nombre;
+    unsigned char byte;
+    long long int tamProb = 0, capacidad = 0;
+
+    int i;
+
+    nombre = (char *)malloc(100 * sizeof(char)); //Reservamos memoria para la cadena del nombre del archivo
+    FILE *datos = fopen("frecuencias.txt", "rb"); //Se abre el archivo frecuencias.txt en forma de bytes
+    fscanf(datos,"%s",nombre); //Se lee el nombre del archivo y se guarda en la variable nombre
+    fscanf(datos, "%lld", &capacidad); //Se lee la cantidad de nodos y se guarda en la variable capacidad
+
+    nodos = (nodo *)malloc(sizeof(nodo) * capacidad); /*Reserva memoria para la cantidad de nodos encontrados en frecuencias.txt*/
+
+    for (i = 0; i < capacidad; i++)//For para recorrer el archivo de frecuencas
+    {
+        fscanf(datos, "%d", &byte); //Se lee el caracter
+        fscanf(datos, "%d", &reps); //Se lee las frecuencias del caracter
+        tamProb += reps; //Aumentamos la variable del tamanio del problema
+        nodos[i] = crearNodo(byte, reps); //Creamos nodos con el caracter y la frecuencia
+    }
+
+    fclose(datos);//Cerramos el archivo
+
+    printf("Nombre\t\t\tTam archivo\n");//Se da formato a la impresion de pantalla
+    printf("%-20s\t\t",nombre);
+    printf("%10d bytes\n",tamProb);
+    arbolHuffman = Huffman(nodos, capacidad);//Creamos el arbol de huffman
+    FILE *archivo = fopen("codificacion.dat", "rb");//Abrimos el archivo codificacion.dat y lo leemos en bytes
+    if(!archivo)
+        perror("No se pudo abrir archivo");
+    FILE *nuevo = fopen(nombre, "wb");//Creamos un archivo nuevo donde recuperaremos el archivo original
+    if(!nuevo)
+        perror("No se pudo abrir nuevo");
+    nodo temp=arbolHuffman; //Creamos un nodo temporal que apunta a la raiz del arbol
+    while(tamProb){ //Mientras no hayamos recuperado el tamanio original del archivo 
+        unsigned char aux = fgetc(archivo);//Le asignamos un caracter a aux
+        for(i=7; i>=0;i--){ //Se realiza un for para analizar cada bit
+            if(CONSULTARBIT(aux,i)) // Consultamos si el bit es 1
+            {
+                temp = temp->derecha; //Se va por la derecha
+            }
+            else if(!CONSULTARBIT(aux,i)) //Consultamos si el bit es 0
+            {
+                temp = temp->izquierda; //Se va por la izquierda
+            } 
+            if(esHoja(temp->izquierda,temp->derecha)) //Revisa si ya llegamos a una hoja o caracter
+            {
+                fwrite(&(temp->caracter),1,sizeof(temp->caracter),nuevo);//Escribe en el archivo el caracter encontrado
+                
+                temp = arbolHuffman;//Se le asigna a temp la raiz del arbol
+            }
+        }
+        tamProb--;//Disminuye el tamanio de problema
+    }
+
+    fclose(archivo);//Cerramos el archivo
+    fclose(nuevo);//Cerramos el archivo
+    
+    uswtime(&utime1, &stime1, &wtime1);//Evalua los tiempos de ejecucion
+    printf("\nreal (Tiempo total)\t user(Tiempo CPU)\t sys (Tiempo E/S)\t CPU/Wall\n");
+	printf("%.10e\t",  wtime1 - wtime0);
+	printf("%.10e\t",  utime1 - utime0);
+	printf("%.10e\t",  stime1 - stime0);
+	printf("%.10f %%\t\n\n",100.0 * (utime1 - utime0 + stime1 - stime0) / (wtime1 - wtime0));
+
+    return 0;
+}
--- huffman.c
+++ huffman.c
@@ -1,76 +1,78 @@
 #include "huffman.h"
 nodo Huffman(nodo *nodos, TIPO tam)
 {
+    //verifica si los nodos no son nulos
+    if(!nodos){
+        perror("Recibe nodos NULL o vacios Huffman(2)");
+        exit(-2);
+    }
+    //inicializa sus ramas
     nodo izquierda, derecha, nuevoNodo;
+    //crea la cola de prioridad (implementacion con Heaps)
     monticulo colaprioridad = construirMinHeap(nodos, tam);
-
-    while (colaprioridad->tam != 1)
+    
+    //mientras el tamaño no sea uno (ultimo arbol creado)
+    while (colaprioridad->tam != 1)  
     {
+        //obtiene el primer elemento de la cola
         izquierda = extractMin(colaprioridad);
+        //obtiene el segundo elemento de la cola
         derecha = extractMin(colaprioridad);
-
+        //crea el nodo con las frecuencas de los elementos obtenidos (su caracter es vacio)
         nuevoNodo = crearNodo('\0', izquierda->frecuencia + derecha->frecuencia);
-
+        //le asigna como hijos (hojas) los elementos obtenidos
         nuevoNodo->izquierda = izquierda;
         nuevoNodo->derecha = derecha;
-
+        //el mini arbol creado, se inserta a la cola de prioridad
         insertar(colaprioridad, nuevoNodo);
     }
+    //obtiene el elemento mas pequeño
     return extractMin(colaprioridad);
 }
-void codigos(TIPO cadenaCodigos[], TIPO n)
+char* Codigos(char cadenaCodigos[], TIPO n)
 {
-    TIPO i;
+    int i;
+    //solicita memoria para la cadena nueva
+    char* cadena=malloc(sizeof(char*)*n);
+    //crea la cadena hasta la posicion ingresada
     for (i = 0; i < n; ++i)
-        printf("%d", cadenaCodigos[i]);
-    printf("\n");
+        cadena[i]= cadenaCodigos[i];
+        //se le asigna al final el caracter vacio
+    cadena[i]='\0';
+    //retorna la cadena creada
+    return cadena;
 }
-void imprimirCodigos(nodo arbolHuffman, TIPO cadenaCodigos[], TIPO posicion)
+void imprimirCodigos(nodo arbolHuffman, char cadenaCodigos[], TIPO posicion, Codigo *codigos)
 {
+    //Valida si el arbol no es nulo
+    if(!arbolHuffman){
+        perror("Recibe un nodo NULL o vacio imprimirCodigos(3)");
+        exit(-2);
+    }
+    //Valida si el arbol tiene una rama izquierda
     if (arbolHuffman->izquierda)
     {
-        cadenaCodigos[posicion] = 0;
-        imprimirCodigos(arbolHuffman->izquierda, cadenaCodigos, posicion + 1);
+        //Asigna a la cadena de codigo un bit '0'
+        cadenaCodigos[posicion] = '0';
+        //Se llama recursivamente a la funcion imprimirCodigos mandando el nodo izquierdo y aumentando la posicion en una unidad
+        imprimirCodigos(arbolHuffman->izquierda, cadenaCodigos, posicion + 1, codigos);
     }
+    //Valida si el arbol tiene una rama derecha
     if (arbolHuffman->derecha)
     {
-        cadenaCodigos[posicion] = 1;
-        imprimirCodigos(arbolHuffman->derecha, cadenaCodigos, posicion + 1);
+        //Asigna a la cadena de codigo un bit '1'
+        cadenaCodigos[posicion] = '1';
+        //Se llama recursivamente a la funcion imprimirCodigos mandando el nodo derecho y aumentando la posicion en una unidad 
+        imprimirCodigos(arbolHuffman->derecha, cadenaCodigos, posicion + 1, codigos);
     }
+    //Valida si la posicion actual correspone a un nodo hoja
     if (esHoja(arbolHuffman->izquierda, arbolHuffman->derecha))
     {
-        printf("%c ", arbolHuffman->caracter);
-        codigos(cadenaCodigos, posicion);
+        //Se reserva memoria dinamica para el codigo del byte que se encuentra en la hoja
+        codigos[arbolHuffman->caracter].cadena = (char *)malloc(sizeof(posicion));
+        //Se almacena el codigo del byte en la hoja
+        codigos[arbolHuffman->caracter].cadena = Codigos(cadenaCodigos, posicion);
+        // se almacena la longitud del codigo del byte correspondiente
+        codigos[arbolHuffman->caracter].longitud = posicion;
     }
 }
-/* void HuffmanCodes(nodo arbolHuffman, apcodigos* codiguitos)
-{
-    nodo temporal;
-    TIPO posicion=0,i=0;
-    char  cadenaCodigos[TAMMAX];
-    temporal=arbolHuffman;
-    strcpy(cadenaCodigos,"");
-    while (temporal)
-    {
-        
-        if (temporal->izquierda)
-        {
-            cadenaCodigos[posicion++] = '0';
-            temporal->izquierda=temporal->izquierda->izquierda;
-        }
-        if (temporal->derecha)
-        {
-            cadenaCodigos[posicion++] = '1';
-            temporal->derecha=temporal->derecha->derecha;
-        }
-        if (esHoja(temporal->izquierda, temporal->derecha))
-        {
-            codiguitos[i]->caracter=temporal->caracter;
-            codiguitos[i]->codigo=cadenaCodigos;
-            codiguitos[i]->frecuencia=temporal->frecuencia;
-            strcpy(cadenaCodigos,"");
-            posicion=0;
-            i++;
-        }
-    } 
-}*/
